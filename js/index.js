3c3bf437dc99cdbbb38fdd387a03194189cfb60c
guozhao <davidkoojohn@163.com>
2019-12-10 23:21:46 +0800

    chore: build demo

--- js/index.js
+++ js/index.js
@@ -1,26 +1,20 @@
-$(function () {
-  const $loading = $('.loading')
-  $.ajax({
-    url: 'http://weather.gtimg.cn/aqi/cityrank.json',
-    dataType: 'jsonp',
-    jsonpCallback: 'cityrank',
-    success: function (res) {
-      $.each(res, function (index, item) {
-        const itemHtml = `<section class="item">
-                          <span class="rank">${index + 1}</span>
-                          <span class="pm-25">${item.pm2_5}</span>
-                          <span class="area">${item.area}</span>
-                          <span class="quality">${item.quality}</span>
-                        </section>`
-        $(itemHtml).appendTo($('.list'))
-      })
-    },
-    error: function (error) {
-      $('<div class="error">数据加载失败，请刷新重试！！！</div>').appendTo($('.list'))
-    },
-    complete: function () {
-      $loading.hide()
-    }
-  })
+import jsonp from 'jsonp'
 
+jsonp('http://weather.gtimg.cn/aqi/cityrank.json', {
+  name: 'cityrank'
+}, (err, data) => {
+  if (data) {
+    data.map((item, index) => {
+      const itemHtml = `<section class="item">
+                        <span class="rank">${index + 1}</span>
+                        <span class="pm-25">${item.pm2_5}</span>
+                        <span class="area">${item.area}</span>
+                        <span class="quality">${item.quality}</span>
+                      </section>`
+      document.querySelector('.list').innerHTML += itemHtml
+    })
+  } else {
+    document.querySelector('.list').innerHTML = '<div class="error">数据加载失败，请刷新重试！！！</div>'
+  }
+  document.querySelector('.loading').style.display = 'none'
 })
